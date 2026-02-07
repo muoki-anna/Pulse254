a373154f9a9eed04be4d5c076e94fe767b4fff7a
Muoki Anna <muokianna10@gmail.com>
2026-02-07 12:19:15 +0300

    add changes on apicongigs

--- Frontend/src/apiconfig/apiconfiguration.ts
+++ Frontend/src/apiconfig/apiconfiguration.ts
@@ -1,3 +1,4 @@
+// apiconfigurations.ts
 export const apiConfig = {
-  backUrl: import.meta.env.VITE_BACK_URL || "https://pulse254-95t7.onrender.com/api"
+  backUrl: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
 };
